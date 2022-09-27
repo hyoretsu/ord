8af0995deecdc04d6722b39843177b1a87f3b582
Aran Leite <hyoretsu@gmail.com>
2022-09-27 16:40:26 -0300

    Aula 2022/9/27

---
+++ Selection Sort/implementation.ts
@@ -0,0 +1,16 @@
+export const selectionSort = (array: number[]): number[] => {
+    array.forEach((element, i) => {
+        let smallestIndex = i;
+
+        array.slice(i + 1).forEach((number, j) => {
+            if (number < array[smallestIndex]) smallestIndex = j + i + 1;
+        });
+
+        if (smallestIndex !== i) {
+            array[i] = array[smallestIndex];
+            array[smallestIndex] = element;
+        }
+    });
+
+    return array;
+};
